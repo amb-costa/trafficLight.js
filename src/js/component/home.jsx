ea777e20d2768958417ab059c81b839e4d0866b3
beinganidiot <ocaranzacosta@gmail.com>
2023-03-18 03:40:24 +0000

    remaking traffic light to show extra purple light +  button for purple light display

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,32 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Home = () => {
-  const [color, setColor] = useState("")
-  return (
+  const [color, setColor] = useState("");
+  const [purple, setPurple] = useState(false);
+
+  return purple ? (
     <div>
-      <div id="stick"/>
-        <div id="traffic">
-          <div onClick={()=>setColor("red")} className={"light red" + (color==="red"? " glow" : "")}></div>
-          <div onClick={()=>setColor("yellow")} className={"light yellow" + (color==="yellow"? " glow" : "")}></div>
-          <div onClick={()=>setColor("green")} className={"light green" + (color==="green"? " glow" : "")}></div>
-        </div>
+      <div id="stick" />
+      <div id="traffic" style={{ height: 540 }}>
+        <div onClick={() => setColor("red")} className={"light red" + (color === "red" ? " glow" : "")}/>
+        <div onClick={() => setColor("yellow")} className={"light yellow" + (color === "yellow" ? " glow" : "")}/>
+        <div onClick={() => setColor("green")} className={"light green" + (color === "green" ? " glow" : "")}/>
+        <div onClick={() => setColor("purple")} className={"light purple" + (color === "purple" ? " glow" : "")}/>
       </div>
+      <h4>Make sure to click a light to make it glow!</h4>
+      <button type="button" onClick={() => setPurple(false)}>Click me to erase the purple light!</button>
+    </div>
+  ) : (
+    <div>
+      <div id="stick" />
+      <div id="traffic">
+        <div onClick={() => setColor("red")} className={"light red" + (color === "red" ? " glow" : "")}/>
+        <div onClick={() => setColor("yellow")} className={"light yellow" + (color === "yellow" ? " glow" : "")}/>
+        <div onClick={() => setColor("green")} className={"light green" + (color === "green" ? " glow" : "")}/>
+      </div>
+      <h4>Make sure to click a light to make it glow!</h4>
+      <button type="button" onClick={() => setPurple(true)}>Click me to add a purple light!</button>
+    </div>
   );
 };
 
